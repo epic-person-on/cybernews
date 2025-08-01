5c5ee8070e11214072e08a17d3ac0a14cc136bbe
Me <123142178+epic-person-on@users.noreply.github.com>
2025-08-01 00:07:09 +0000

    Basic prototype

---
+++ main.py
@@ -0,0 +1,8 @@
+from tools.news import *
+
+def main():
+    print(data_breach())
+
+
+if __name__ == "__main__":
+    main()
---
+++ tools/news.py
@@ -0,0 +1,18 @@
+from cybernews.cybernews import CyberNews
+
+news = CyberNews()
+
+def malware():
+    return news.get_news("malware")
+
+def data_breach():
+    return news.get_news("dataBreach") 
+
+def cyber_attack():
+    return news.get_news("cyberAttack")
+
+def vulnerability():
+    return news.get_news("vulnerability")
+
+def security():
+    return news.get_news("security")
